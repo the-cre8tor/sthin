3115dc0dd3ad5fb5952ea5744deb897994dea80c
Alexander Nitiola <anitiola@aellacredit.com>
2025-03-26 09:02:02 +0100

    created the de url database model

--- src/infrastructure/database/connection.rs
+++ src/infrastructure/database/connection.rs
@@ -1,10 +1,12 @@
-use crate::configuration::DatabaseSettings;
+use crate::{configuration::DatabaseSettings, error::AppError};
 use sqlx::postgres::{PgPool, PgPoolOptions};
 
 pub struct Database;
 
 impl Database {
-    pub async fn establish_connection(config: &DatabaseSettings) -> Result<PgPool, sqlx::Error> {
-        PgPoolOptions::new().connect_lazy(&config.connection_string())
+    pub async fn establish_connection(config: &DatabaseSettings) -> Result<PgPool, AppError> {
+        PgPoolOptions::new()
+            .connect_lazy(&config.connection_string())
+            .map_err(|error| AppError::DatabaseConnectionError(error.to_string()))
     }
 }
---
+++ src/infrastructure/database/models/db_url.rs
@@ -0,0 +1,40 @@
+use chrono::{DateTime, Utc};
+use sqlx::prelude::FromRow;
+use uuid::Uuid;
+
+use crate::domain::{
+    errors::DomainError,
+    models::Url,
+    value_objects::{ShortCode, ValidUrl},
+};
+
+#[derive(Debug, Clone, FromRow)]
+pub struct DbUrl {
+    pub id: Uuid,
+    pub original_url: String,
+    pub short_code: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
+
+impl DbUrl {
+    pub fn to_domain(&self) -> Result<Url, DomainError> {
+        Ok(Url {
+            id: Some(self.id),
+            original_url: ValidUrl::new(self.original_url.clone())?,
+            short_code: ShortCode::new(Some(self.short_code.clone()))?,
+            created_at: Some(self.created_at),
+            updated_at: Some(self.updated_at),
+        })
+    }
+
+    pub fn from_domain(domain_url: &Url) -> Self {
+        Self {
+            id: domain_url.id.unwrap_or_else(|| Uuid::new_v4()),
+            original_url: String::from(domain_url.original_url.as_ref()),
+            short_code: String::from(domain_url.short_code.as_ref()),
+            created_at: domain_url.created_at.unwrap_or_else(Utc::now),
+            updated_at: domain_url.updated_at.unwrap_or_else(Utc::now),
+        }
+    }
+}
