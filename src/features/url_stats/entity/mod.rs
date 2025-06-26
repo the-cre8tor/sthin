4ffc0cb6685c313c34497d7c1db06b923570b459
Alexander Nitiola <anitiola@aellacredit.com>
2025-06-26 05:02:54 +0100

    added url stats entity and model to the list of url_stats module

--- src/features/url_stats/entity/mod.rs
+++ src/features/url_stats/entity/mod.rs
@@ -1,5 +1,7 @@
 mod url_stats_entity;
 mod url_stats_log_entity;
+mod url_stats_report_entity;
 
 pub use url_stats_entity::*;
 pub use url_stats_log_entity::*;
+pub use url_stats_report_entity::*;
--- src/features/url_stats/model/mod.rs
+++ src/features/url_stats/model/mod.rs
@@ -1,5 +1,7 @@
 mod url_stats_logs_model;
 mod url_stats_model;
+mod url_stats_report_model;
 
 pub use url_stats_logs_model::*;
 pub use url_stats_model::*;
+pub use url_stats_report_model::*;
