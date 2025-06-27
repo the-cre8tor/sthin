08a918a8826661cf0e2beb54a204c984e9179afe
Alexander Nitiola <anitiola@aellacredit.com>
2025-06-27 07:49:27 +0100

    deleted the transaction associated function on DatabasePool struct

--- src/infrastructure/database/connection.rs
+++ src/infrastructure/database/connection.rs
@@ -1,8 +1,5 @@
 use crate::{configuration::DatabaseSettings, error::AppError};
-use sqlx::{
-    Postgres, Transaction,
-    postgres::{PgPool, PgPoolOptions},
-};
+use sqlx::postgres::{PgPool, PgPoolOptions};
 
 pub struct DatabasePool {
     pub pool: PgPool,
@@ -13,25 +10,5 @@ impl DatabasePool {
         let pool = PgPoolOptions::new().connect_lazy(&config.connection_string())?;
 
         Ok(Self { pool })
-    }
-
-    pub async fn transaction<F, Fut, R, E>(&self, callback: F) -> Result<R, E>
-    where
-        F: FnOnce(&mut Transaction<'_, Postgres>) -> Fut,
-        Fut: Future<Output = Result<R, E>>,
-        E: From<sqlx::Error>,
-    {
-        let mut tx = self.pool.begin().await?;
-
-        match callback(&mut tx).await {
-            Ok(result) => {
-                tx.commit().await?;
-                Ok(result)
-            }
-            Err(e) => {
-                tx.rollback().await?;
-                Err(e)
-            }
-        }
     }
 }
